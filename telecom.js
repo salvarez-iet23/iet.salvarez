f80f96c3c113b82256d24c0fdd14bd81e657dae3
salvarez-iet23 <also220696@upemor.edu.mx>
2026-02-13 13:12:38 -0600

    Update telecom.js

--- telecom.js
+++ telecom.js
@@ -1,11 +1,8 @@
 document.addEventListener("DOMContentLoaded",()=>{
 
-setTimeout(()=>{
-document.getElementById("loader").style.display="none";
-},1500);
+setTimeout(()=>loader.style.display="none",1500);
 
 window.toggleExclusive=function(header){
-
 let card=header.parentElement;
 
 document.querySelectorAll(".accordion-card")
@@ -14,17 +11,13 @@ if(c!==card)c.classList.remove("active");
 });
 
 card.classList.toggle("active");
-}
+};
 
 /* WhatsApp */
-document.getElementById("contactForm").addEventListener("submit",e=>{
+contactForm.addEventListener("submit",e=>{
 e.preventDefault();
 
-let n=nombre.value;
-let c=correo.value;
-let m=mensaje.value;
-
-window.open(`https://wa.me/527771060169?text=Nombre:${n}%0ACorreo:${c}%0AMensaje:${m}`);
+window.open(`https://wa.me/527771060169?text=Nombre:${nombre.value}%0ACorreo:${correo.value}%0AMensaje:${mensaje.value}`);
 });
 
 /* ScrollSpy */
@@ -32,14 +25,10 @@ let sections=document.querySelectorAll(".section");
 let links=document.querySelectorAll("#navbar a");
 
 window.addEventListener("scroll",()=>{
-
 let current="";
 
 sections.forEach(sec=>{
-let top=window.scrollY;
-let offset=sec.offsetTop-150;
-
-if(top>=offset)current=sec.id;
+if(window.scrollY>=sec.offsetTop-150)current=sec.id;
 });
 
 links.forEach(a=>{
@@ -49,11 +38,34 @@ if(a.getAttribute("href")==="#"+current)a.classList.add("active");
 });
 
 /* Cursor WiFi */
-let cursor=document.querySelector(".wifi-cursor");
-
 document.addEventListener("mousemove",e=>{
-cursor.style.left=e.clientX+"px";
-cursor.style.top=e.clientY+"px";
+document.querySelector(".wifi-cursor").style.left=e.clientX+"px";
+document.querySelector(".wifi-cursor").style.top=e.clientY+"px";
 });
+
+/* Fondo nodos */
+const net=networkCanvas.getContext("2d");
+networkCanvas.width=window.innerWidth;
+networkCanvas.height=window.innerHeight;
+
+let nodes=[];
+for(let i=0;i<35;i++){
+nodes.push({x:Math.random()*networkCanvas.width,
+y:Math.random()*networkCanvas.height});
+}
+
+function drawNodes(){
+net.clearRect(0,0,networkCanvas.width,networkCanvas.height);
+
+nodes.forEach(n=>{
+net.beginPath();
+net.arc(n.x,n.y,2,0,Math.PI*2);
+net.fillStyle="cyan";
+net.fill();
+});
+
+requestAnimationFrame(drawNodes);
+}
+drawNodes();
 
 });
